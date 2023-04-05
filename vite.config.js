199763c69ce45cb3fb0be47e8766fde16a2c1b14
Viswa Gullapalli <aubertlone@gmail.com>
2023-04-05 18:53:15 -0500

    minor typo

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     open: '/',
     proxy: {
       '/api': {
-           target: 'http`://localhost:3000',
+           target: 'http://localhost:3000',
            changeOrigin: true,
            secure: false,      
            ws: true,
