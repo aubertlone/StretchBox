3889b7b8142c9e5f25e2a52508321e7efd48efc0
Viswa Gullapalli <aubertlone@gmail.com>
2023-04-09 15:38:42 -0500

    app rewrite

--- client/actions/actionTypes.js
+++
@@ -1,2 +0,0 @@
-// create action types here
-// EXAMPLE: export const ACTION_NAME = 'ACTION_NAME';
--- client/reducers/masterReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-// IMPORT ALL REDUCERS HERE
-import stretchesReducer from './stretchesReducer';
-
-const reducers = combineReducers({
-  stretches: stretchesReducer,
-});
-
-export default reducers;
